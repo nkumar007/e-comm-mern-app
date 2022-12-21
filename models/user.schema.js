06ee55317d74a03d6c863247ceb40025f3dc693c
nkumar007 <naveen.mbuff98@gmail.com>
2022-12-21 20:58:04 +0530

    added user model

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
 
 const userSchema = mongoose.Schema({
   name: {
@@ -6,6 +7,22 @@ const userSchema = mongoose.Schema({
     required: [true, "Name is required"],
     maxLength: [50, "Name must be less than 50 charecters"],
   },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    minLength: [8, "password must be atleast of 8 charecters"],
+    select: false,
+  },
+  role: {
+    type: String,
+    enum: Object.values(AuthRoles),
+    default: AuthRoles.USER,
+  },
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("User", userSchema);
